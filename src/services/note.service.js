29b0e8eab807c196c4632949f88c5ff721bee7fd
windyeasy <windyeasy13gb@163.com>
2024-01-07 19:39:19 +0800

    fix: 修复添加文章bug

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -1,11 +1,6 @@
 const connection = require("../app/database");
 const BaseService = require("./base.service");
 
-class NoteService extends BaseService {
-  async create(content) {
-    const statement = "";
-    connection.execute(statement, [content]);
-  }
-}
+class NoteService extends BaseService {}
 const noteService = new NoteService("note");
 module.exports = noteService;
